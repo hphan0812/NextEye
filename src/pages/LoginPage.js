6853073c17c999314831a0dfce13d682ebe098dd
hphan0812 <honghanh.phan@nextrobotics.io>
2024-06-03 11:41:02 +0700

    [FEAT] hphan: update file for project

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,19 +5,6 @@ import { Input } from "components/Input";
 import { useForm } from "react-hook-form";
 import FormGroup from "components/Common/FormGroup";
 import { Button } from "components/Button";
-// import * as yup from "yup";
-// import { yupResolver } from "@hookform/resolvers/yup";
-
-// const schema = yup.object()({
-//     email: yup
-//         .string()
-//         .email("Please enter a valid email")
-//         .required("Email is required"),
-//     password: yup
-//         .string()
-//         .min(8, "Password must be at least 8 characters")
-//         .required("Password is required"),
-// });
 
 const LoginPage = () => {
   const {
