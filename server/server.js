fdd295ca95e6be4e112cde627a87d175ac41cae1
hphan0812 <honghanh.phan@nextrobotics.io>
2024-06-07 15:06:55 +0700

    [FEAT] hphan: up load server test

--- server/server.js
+++ server/server.js
@@ -1,1 +1,29 @@
 
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+dotenv.config();
+const app = express();
+
+mongoose.set('strictQuery', true);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => {
+    console.error('Could not connect to MongoDB', err);
+    process.exit(1);
+});
+
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
